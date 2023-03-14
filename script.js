4d618b2ae394c76c1f97ff5619d48010f90f30d1
Rizky <61013149+rizky-cmd@users.noreply.github.com>
2023-03-14 10:16:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+// variabel 1. Buah
+let buah = 'Apel';
+function setBuah(argBuah) {
+    buah = argBuah()
+}
+function getBuah(setBuah) {
+    return buah
+}
+console.log(getBuah(buah))
+// variabel 2. Warna
+let warna = 'Merah';
+function setWarna(argWarna) {
+    warna = argWarna()
+}
+function getWarna(setWarna) {
+    return warna
+}
+console.log(getWarna(warna))
+// variabel 3. Sayur
+let sayur = 'Bayam';
+function setSayur(argSayur) {
+    sayur = argSayur()
+}
+function getSayur(setSayur) {
+    return sayur
+}
+console.log(getSayur(sayur))
+// variabel 4. Baju
+let baju = 'Kaos';
+function setBaju(argBaju) {
+    baju = argBaju()
+}
+function getBaju(setBaju) {
+    return baju
+}
+console.log(getBaju(baju))
+// variabel 5. Celana
+let celana = 'Jeans';
+function setCelana(argCelana) {
+    celana = argCelana()
+}
+function getCelana(setCelana) {
+    return celana
+}
+console.log(getCelana(celana))
+// variabel 6. Hewan
+let hewan = 'Kucing';
+function setHewan(argHewan) {
+    hewan = argHewan()
+}
+function getHewan(setHewan) {
+    return hewan
+}
+console.log(getHewan(hewan))
+// variabel 7. Suhu
+let suhu = 'Celcius';
+function setSuhu(argSuhu) {
+    suhu = argSuhu()
+}
+function getSuhu(setSuhu) {
+    return suhu
+}
+console.log(getSuhu(suhu))
+// variabel 8. Mobil
+let mobil = 'Lamborghini';
+function setMobil(argMobil) {
+    mobil = argMobil()
+}
+function getMobil(setMobil) {
+    return mobil
+}
+console.log(getMobil(mobil))
+// variabel 9. Motor
+let motor = 'Vanigale V4s';
+function setMotor(argMotor) {
+    motor = argMotor()
+}
+function getMotor(setMotor) {
+    return motor
+}
+console.log(getMotor(motor))
+// variabel 10. Uang
+let uang = 'Rupiah';
+function setUang(argUang) {
+    uang = argUang()
+}
+function getUang(setUang) {
+    return uang
+}
+console.log(getUang(uang))
+// variabel 11. Sepatu
+let sepatu = 'Ventella';
+function setSepatu(argSepatu) {
+    sepatu = argSepatu()
+}
+function getSepatu(setSepatu) {
+    return sepatu
+}
+console.log(getSepatu(sepatu))
+// variabel 12. Cuaca
+let cuaca = 'Musim Salju';
+function setCuaca(argCuaca) {
+    cuaca = argCuaca()
+}
+function getCuaca(setCuaca) {
+    return cuaca
+}
+console.log(getCuaca(cuaca))
+// variabel 13. Coklat
+let coklat = 'Silverqueen';
+function setCoklat(argCoklat) {
+    coklat = argCoklat()
+}
+function getCoklat(setCoklat) {
+    return coklat
+}
+console.log(getCoklat(coklat))
+// variabel 14. Bunga
+let bunga = 'Mawar';
+function setBunga(argBunga) {
+    bunga = argBunga()
+}
+function getBunga(setBunga) {
+    return bunga
+}
+console.log(getBunga(bunga))
+// variabel 15. Topi
+let topi = 'Bucket';
+function setTopi(argTopi) {
+    topi = argTopi()
+}
+function getTopi(setTopi) {
+    return topi
+}
+console.log(getTopi(topi))
+// variabel 16. HP
+let hp = 'Android';
+function setHp(argHp) {
+    hp = argHp()
+}
+function getHp(setHp) {
+    return hp
+}
+console.log(getHp(hp))
+// variabel 17. Negara
+let negara = 'Indonesia';
+function setNegara(argNegara) {
+    negara = argNegara()
+}
+function getNegara(setNegara) {
+    return negara
+}
+console.log(getNegara(negara))
+// variabel 18. Makanan
+let makanan = 'Nasi Goreng';
+function setMakanan(argMakanan) {
+    makanan = argMakanan()
+}
+function getMakanan(setMakanan) {
+    return makanan
+}
+console.log(getMakanan(makanan))
+// variabel 19. Minuman
+let minuman = 'Starbuck';
+function setMinuman(argMinuman) {
+    minuman = argMinuman()
+}
+function getMinuman(setMinuman) {
+    return minuman
+}
+console.log(getMinuman(minuman))
+// variabel 20. Jam
+let jam = 'Casio';
+function setJam(argJam) {
+    jam = argJam()
+}
+function getJam(setJam) {
+    return jam
+}
+console.log(getJam(jam))
